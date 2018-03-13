59e95fb71109dab3f0da6b1501f4ca049581c344
Ruth Torres Castillo <rutorrres@nmsu.edu>
2018-03-12 23:16:06 -0600

    Finish design for Map display

---
+++ MidTermApp/MapViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MapViewController.swift
+//  MidTermApp
+//
+//  Created by Ruth Torres Castillo on 3/12/18.
+//  Copyright © 2018 New Mexico State University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+import CoreData
+
+class MapViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var managedObjectContext: NSManagedObjectContext!
+    
+    // MARK:- Actions
+    @IBAction func showUser() {
+        let region = MKCoordinateRegionMakeWithDistance(mapView.userLocation.coordinate, 1000, 1000)
+        mapView.setRegion(mapView.regionThatFits(region), animated: true)
+    }
+    
+    @IBAction func showLocations() {
+       
+    }
+}
+   
+   
+extension MapViewController: MKMapViewDelegate {
+    
+}
+
