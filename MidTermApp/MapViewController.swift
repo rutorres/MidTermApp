881ced19e9618ccca56c80bec5f4b253cd3d89a3
Ruth Torres Castillo <rutorrres@nmsu.edu>
2018-03-15 19:51:10 -0600

    Finish database for Route1

--- MidTermApp/MapViewController.swift
+++ MidTermApp/MapViewController.swift
@@ -16,9 +16,8 @@ class MapViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     var stops = [Stops]()
-    var managedObjectContext: NSManagedObjectContext!
-    var identifier = ""
-    /*var managedObjectContext: NSManagedObjectContext! {
+    //sets up an observer to the Object for when changes happen
+    var managedObjectContext: NSManagedObjectContext! {
         didSet {
             NotificationCenter.default.addObserver(forName: Notification.Name.NSManagedObjectContextObjectsDidChange, object: managedObjectContext, queue: OperationQueue.main) { notification in
                 if self.isViewLoaded {
@@ -26,15 +25,16 @@ class MapViewController: UIViewController {
                 }
             }
         }
-    }*/
+    }
+    var identifier = ""
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //showUser()
         updateStops()
         if !stops.isEmpty {
             showLocations()
+            //drawRegion(for: stops)
         }
     }
 
@@ -50,17 +50,13 @@ class MapViewController: UIViewController {
         //let span = MKCoordinateSpanMake(0.05, 0.05)
        // let region = MKCoordinateRegion(center: location, span: span)
         //mapView.setRegion(region, animated: true)
-        //let annotation = MKPointAnnotation()
-        //annotation.coordinate = location
-        //annotation.title = "Stop 1"
-       // annotation.subtitle = "Mesilla Valley"
-        //mapView.addAnnotation(annotation)
+
         let theRegion = region(for: stops)
         mapView.setRegion(theRegion, animated: true)
 
     }
     @IBAction func showRoutes(){
-        mapView.delegate = self
+        mapView.delegate = self as MKMapViewDelegate
         let sourceLocation = CLLocationCoordinate2D(latitude: 32.305377, longitude: -106.780586)
         let destinationLocation = CLLocationCoordinate2D(latitude: 32.312349, longitude: -106.778191)
         let sourcePlacemark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil)
@@ -68,18 +64,6 @@ class MapViewController: UIViewController {
         let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
         let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
         
-        /*let sourceAnnotation = MKPointAnnotation()
-        sourceAnnotation.title = "Times Square"
-        if let location = sourcePlacemark.location {
-            sourceAnnotation.coordinate = location.coordinate
-        }
-        let destinationAnnotation = MKPointAnnotation()
-        destinationAnnotation.title = "Empire State Building"
-        if let location = destinationPlacemark.location {
-            destinationAnnotation.coordinate = location.coordinate
-        }*/
-        // 6.
-        //self.mapView.showAnnotations([sourceAnnotation,destinationAnnotation], animated: true )
         let directionRequest = MKDirectionsRequest()
         directionRequest.source = sourceMapItem
         directionRequest.destination = destinationMapItem
@@ -95,19 +79,13 @@ class MapViewController: UIViewController {
                 return
             }
             let route = response.routes[0]
-            //drawing the response
-            //let line = MKPolyline(coordinates: [sourceLocation,destinationLocation], count: 2)
-           // self.mapView.add(line, level: MKOverlayLevel.aboveRoads)
-            //let polyline: MKPolyline = MKPolyline()
-            //polyline.tag = 100 //any number that you will use to identify this overlay
+            
             print(route.description)
             self.identifier = route.name
             print(self.identifier)
             print("\n")
-            
-           // self.mapView.add(polyline)
+ 
             self.mapView.add((route.polyline), level: MKOverlayLevel.aboveRoads)
-            //let rect = line.boundingMapRect
             let rect = route.polyline.boundingMapRect
             self.mapView.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
         }
@@ -116,64 +94,84 @@ class MapViewController: UIViewController {
         //mapView.removeAnnotations(stops as! [MKAnnotation])
         
         for overlay in mapView.overlays {
-            print(overlay.description)
-            print("\(overlay.title)")
-            print("\n")
-            if "SChurchSt" == overlay.title as! String {
+            //print(overlay.description)
+            //print("\(overlay.title)")
+            //print("\n")
+            //if "SChurchSt" == overlay.title as! String {
             mapView.remove(overlay)
+            //}
+        }
+       
+    }
+    
+    @IBAction func drawRegion() {
+        
+        for i in 0..<(stops.count-1){
+            mapView.delegate = self
+            let source = stops[i]
+            let destination = stops[i+1]
+            
+            let sourceLocation = CLLocationCoordinate2D(latitude: source.latitude, longitude: source.longitude)
+            print(sourceLocation)
+            let destinationLocation = CLLocationCoordinate2D(latitude: destination.latitude, longitude: destination.longitude)
+            print(destinationLocation)
+            print("\n")
+            
+            let sourcePlacemark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil)
+            let destinationPlacemark = MKPlacemark(coordinate: destinationLocation, addressDictionary: nil)
+            let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
+            let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+            let directionRequest = MKDirectionsRequest()
+            directionRequest.source = sourceMapItem
+            directionRequest.destination = destinationMapItem
+            directionRequest.transportType = .automobile
+            // Calculate the direction
+            let directions = MKDirections(request: directionRequest)
+            
+            directions.calculate {
+                (response, error) -> Void in
+                guard let response = response else {
+                    if let error = error {
+                        print("Error: \(error)")
+                    }
+                    return
+                }
+                let route = response.routes[0]
+                
+                print(route.description)
+                self.identifier = route.name
+                print(self.identifier)
+                print("\n")
+                
+                self.mapView.add((route.polyline), level: MKOverlayLevel.aboveRoads)
+                //let rect = route.polyline.boundingMapRect
+                let location = CLLocationCoordinate2D(latitude: 32.326595,
+                                                      longitude: -106.775436)
+                let span = MKCoordinateSpanMake(0.05, 0.05)
+                let region = MKCoordinateRegion(center: location, span: span)
+                
+                self.mapView.setRegion(region, animated: true)
+                
             }
         }
-        /*mapView.delegate = self
-        let sourceLocation = CLLocationCoordinate2D(latitude: 32.305377, longitude: -106.780586)
-        let destinationLocation = CLLocationCoordinate2D(latitude: 32.312349, longitude: -106.778191)
-        let sourcePlacemark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil)
-        let destinationPlacemark = MKPlacemark(coordinate: destinationLocation, addressDictionary: nil)
-        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
-        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
-        let directionRequest = MKDirectionsRequest()
-        directionRequest.source = sourceMapItem
-        directionRequest.destination = destinationMapItem
-        directionRequest.transportType = .automobile
-        // Calculate the direction
-        let directions = MKDirections(request: directionRequest)
-        directions.calculate {
-            (response, error) -> Void in
-            guard let response = response else {
-                if let error = error {
-                    print("Error: \(error)")
-                }
-                return
-            }
-            let route = response.routes[0]
-            //drawing the response
-            self.mapView.add((route.polyline), level: MKOverlayLevel.aboveRoads)
-            let rect = route.polyline.boundingMapRect
-            self.mapView.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
-    
-            
-        }*/
     }
    
-    @IBAction func showLocationDetails(_ sender: UIButton) {
-        
-    }
-    
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let renderer = MKPolylineRenderer(overlay: overlay)
-        renderer.strokeColor = UIColor.blue
+        renderer.strokeColor = UIColor.green
         renderer.lineWidth = 4.0
         return renderer
     }
     //MARK:- Private methods
     func updateStops(){
-        mapView.removeAnnotations(stops as! [MKAnnotation])
+        mapView.removeAnnotations(stops as [MKAnnotation])
         let entity = Stops.entity()
         let fetchRequest = NSFetchRequest<Stops>()
         fetchRequest.entity = entity
         stops = try! managedObjectContext.fetch(fetchRequest)
         mapView.addAnnotations(stops)
-        
     }
+    
     func region(for annotations: [MKAnnotation]) -> MKCoordinateRegion {
         let region: MKCoordinateRegion
         
@@ -199,44 +197,52 @@ class MapViewController: UIViewController {
             
             let center = CLLocationCoordinate2D(latitude: topLeft.latitude - (topLeft.latitude - bottomRight.latitude) / 2, longitude: topLeft.longitude - (topLeft.longitude - bottomRight.longitude) / 2)
             
-            let extraSpace = 1.1
-            /*let span = MKCoordinateSpan(latitudeDelta: abs(topLeft.latitude - bottomRight.latitude) * extraSpace, longitudeDelta: abs(topLeft.longitude - bottomRight.longitude) * extraSpace)
+            let extraSpace = 2.0
+            let span = MKCoordinateSpan(latitudeDelta: abs(topLeft.latitude - bottomRight.latitude) * extraSpace, longitudeDelta: abs(topLeft.longitude - bottomRight.longitude) * extraSpace)
+            region = MKCoordinateRegion(center: center, span: span)
             
-            region = MKCoordinateRegion(center: center, span: span)*/
-            
-            let location = CLLocationCoordinate2D(latitude: 32.305377, longitude: -106.780586)
+            /*let location = CLLocationCoordinate2D(latitude: 32.305377, longitude: -106.780586)
             let span = MKCoordinateSpanMake(0.03, 0.03)
-            region = MKCoordinateRegion(center: location, span: span)
+            region = MKCoordinateRegion(center: location, span: span)*/
         }
         
         return mapView.regionThatFits(region)
     }
+    
+    @objc func showStopDetails(_ sender: UIButton) {
+        performSegue(withIdentifier: "EditLocation", sender: sender)
+    }
+    
+    
 
 }
    
    
 extension MapViewController: MKMapViewDelegate {
-
-    /*func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        let identifier = "location"
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is Stops else {
+            return nil
+        }
+        let identifier = "stops"
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
         if annotationView == nil {
             let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
             pinView.isEnabled = true
             pinView.canShowCallout = true
             pinView.animatesDrop = false
-            pinView.pinTintColor = UIColor(red: 0.32, green: 0.82, blue: 0.4, alpha: 1)
+            pinView.pinTintColor = UIColor(red: 0.0, green: 0.82, blue: 0.4, alpha: 1)
             let rightButton = UIButton(type: .detailDisclosure)
-            rightButton.addTarget(self, action: #selector(showLocationDetails), for: .touchUpInside)
+            rightButton.addTarget(self, action: #selector(showStopDetails), for: .touchUpInside)
             pinView.rightCalloutAccessoryView = rightButton
             annotationView = pinView
+        }
+        if let annotationView = annotationView {
+            annotationView.annotation = annotation
+            let button = annotationView.rightCalloutAccessoryView as! UIButton
+            if let index = stops.index(of: annotation as! Stops) {
+                button.tag = index
+            }
+        }
+        return annotationView
     }
-    if let annotationView = annotationView {
-        annotationView.annotation = annotation
-        let button = annotationView.rightCalloutAccessoryView as! UIButton
-    }
-    return annotationView
-    }*/
-
 }
-
